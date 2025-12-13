5ae672c386472a84c18864d38f63c9db9a4dac87
xxxmoney <xxxmoney111@gmail.com>
2025-12-13 14:41:14 +0100

    Improved documentation, small fix for modal

--- src/app.py
+++ src/app.py
@@ -220,10 +220,7 @@ app.layout = html.Div([
     # Modal
     dbc.Modal([
         dbc.ModalHeader(dbc.ModalTitle("Vehicle Detail"), close_button=True),
-        dbc.ModalBody(id="modal-body"),
-        dbc.ModalFooter(
-            dbc.Button("Close", id="close-modal", className="ms-auto", n_clicks=0)
-        ),
+        dbc.ModalBody(id="modal-body")
     ], id="car-modal", is_open=False, size="lg", centered=True),
 
 ], style={"backgroundColor": "#f8f9fa", "minHeight": "100vh"})
